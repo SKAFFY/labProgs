b56c5599e5a12b7c9f958bc1920c3092f0d8763a
Михаил <kheyfets03@mail.ru>
2021-11-04 21:24:46 +0300

    lab 8
    try 3

--- 04.11.21 lab 8 prog/main.cpp
+++ 04.11.21 lab 8 prog/main.cpp
@@ -2,29 +2,41 @@
 
 using namespace std;
 
-
+int sizStr(char str[])
+{
+    int c = 0;
+    while(str[c] != '\0')
+    {
+        c+=1;
+    }
+    return c+1;
+}
 
 int main()
 {
     // 1 4 7 8 11
     // BEGIN concatenatio
-    char str1[] = "russia is";
-    char str2[] = " the best";
-    int siz = sizeof(str1) + sizeof(str2);
+    char str1[100]; // = "russia is";
+    scanf(" %s", str1);
+    char str2[100]; // = " the best";
+    scanf(" %s", str2);
+    int siz = sizStr(str1) + sizStr(str2);
     char* sumStr = (char*)malloc(siz*sizeof(char));
     int i = 0;
-    for(i; i< sizeof(str1)-1; i++)
+    for(i; i< sizStr(str1)-1; i++)
         sumStr[i] = str1[i];
-    for(int j = 0; j < sizeof(str2); j++)
+    for(int j = 0; j < sizStr(str2); j++)
         sumStr[i+j] = str2[j];
-    printf("%s\n", sumStr);
+    printf("\n%s\n", sumStr);
     //END concatenatio
     // BEGIN Ncomparison
-    char str3[] = "abcdi";
-    char str4[] = "abcdj";
+    char str3[100]; // = "abcdi";
+    scanf(" %s", str3);
+    char str4[100]; // = "abcdj";
+    scanf(" %s", str4);
     int N = 4; // количество сравниваемых символов
     int m = 0, n = 0;
-    while((m < sizeof(str3)) && (n < sizeof(str4)))
+    while((m < sizStr(str3)) && (n < sizStr(str4)))
     {
         if(int(str3[m]) == int(str4[n]))
         {
@@ -46,20 +58,24 @@ int main()
         }
     }
     // END Ncomparison
-    // BEGIN mineSIZEOF
-    char str5[] = "primer sredney stroky s probelamy";  // если нужно сделать ввод с консоли, то вот нужна€ конструкци€
-                                                        // scanf("%[^\n]s", &str5);
-    int counter = 0;
+    // BEGIN minesizStr
+    char str5[100]; // = "primer sredney stroky s probelamy";  // если нужно сделать ввод с консоли, то вот нужна€ конструкци€
+    scanf(" %s", str5);                                       // scanf("%[^\n]s", &str5);
+    /*int counter = 0;
     while(str5[counter] != '\0')
         counter +=1;
-    printf("\nsize of str = %d\n", counter);
-    // END mineSIZEOF
+    */
+
+    printf("\nsize of str = %d\n", sizStr(str5));
+    // END minesizStr
     // BEGIN firstEnter
-    char str6[] = "laboratornaya po programmirovaniy";
-    char need = ' '; // искомый символ
+    char str6[100]; // = "laboratornaya po programmirovaniy";
+    scanf(" %s", str6);
+    char need; // = ' '; // искомый символ
+    scanf(" %c", &need);
     int pos, flag = 0;
     i = 0;
-    while(i < sizeof(str6))
+    while(i < sizStr(str6))
     {
         if(str6[i] == need)
         {
@@ -76,10 +92,12 @@ int main()
         printf("\npos of \"%c\" = %d\n", need, pos);
     // END firstEnter
     // BEGIN отрезок
-    char str7[] = "Meet my family. There are five of us Ц my parents, my elder brother, my baby sister and me. "; // строка в которой ищем
-    char str8[] = " 123TtHhRrEe"; // алфавит, отрезок из которого ищем
+    char str7[100]; // = "Meet my family. There are five of us Ц my parents, my elder brother, my baby sister and me. "; // строка в которой ищем
+    scanf(" %s", str7);
+    char str8[100]; // = " 123TtHhRrEe"; // алфавит, отрезок из которого ищем
+    scanf(" %s", str8);
     int c = 0, maxC = 0;
-    int siz7 = sizeof(str7), siz8 = sizeof(str8);
+    int siz7 = sizStr(str7), siz8 = sizStr(str8);
     for(int i = 0; i < siz7; i+=1)
     {
         flag = 0;
